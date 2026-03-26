1a5f7ce3b7fc948390210f96a74a02a880f94b88
Camilo Andres Matteo Perez <camilo.matteo@MacBook-Pro-de-Administrador-3.local>
2026-03-25 23:01:16 -0300

    :sparkles: implement masonry grid and remember scroll value

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,12 +1,44 @@
-import { Component, inject } from '@angular/core';
-import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { GifService } from '../../services/gif.service';
+import { ScrollStateService } from '../../shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
+  onScroll = (event: Event) => {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop; // distance from top
+    const clientHeight = scrollDiv.clientHeight; // total pixel viewport
+    const scrollHeight = scrollDiv.scrollHeight; // max limit to make scroll
+
+    const isAtBottomViewport = scrollTop + clientHeight + 300 >= scrollHeight;
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
+    if (isAtBottomViewport) {
+      this.gifService.getTrendingGifs();
+    }
+  };
 }
--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,38 +1,65 @@
 import { HttpClient } from '@angular/common/http';
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@envs/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interface';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
 import { map, Observable, tap } from 'rxjs';
+import { chunkArray } from '../shared/utils';
+
+const GIFS_LOCAL_STORAGE_KEY = 'gifs';
+const LIMIT_PER_GIFS = 20;
+
+function getGifsFromLocalStorage() {
+  const results = localStorage.getItem(GIFS_LOCAL_STORAGE_KEY) ?? '{}';
+  const gifs = JSON.parse(results);
+
+  return gifs;
+}
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
+  private trendingPage = signal<number>(0);
+
   trendingGifs = signal<Gif[]>([]);
-  isLoading = signal<boolean>(true);
-  gifsHistory = signal<Record<string, Gif[]>>({});
+  isLoadingTrendingGifs = signal<boolean>(false);
+  gifsHistory = signal<Record<string, Gif[]>>(getGifsFromLocalStorage());
   gifsHistoryKeys = computed(() => Object.keys(this.gifsHistory()));
+  isLoading = signal<boolean>(false);
 
   constructor() {
     this.getTrendingGifs();
   }
 
+  saveGifsOnStorage = effect(() => {
+    const historyString = JSON.stringify(this.gifsHistory());
+    localStorage.setItem(GIFS_LOCAL_STORAGE_KEY, historyString);
+  });
+
+  trendinfGifsGroup = computed<Gif[][]>(() =>
+    chunkArray(this.trendingGifs(), 3),
+  );
+
   getTrendingGifs() {
+    if (this.isLoadingTrendingGifs()) return;
+    this.isLoadingTrendingGifs.set(true);
+
     this.http
       .get<GiphyResponse>(`${environment.giphyURL}/gifs/trending`, {
         params: {
           api_key: environment.giphyApiKey,
-          limit: 20,
-          offset: 0,
+          limit: LIMIT_PER_GIFS,
+          offset: this.trendingPage() * LIMIT_PER_GIFS,
           rating: 'g',
           bundle: 'messaging_non_clips',
         },
       })
       .subscribe((response) => {
         const gifs = GifMapper.mapGiphyItemArrayToGifArray(response.data);
-        this.trendingGifs.set(gifs);
-        this.isLoading.set(false);
+        this.trendingGifs.update((prevGifs) => [...prevGifs, ...gifs]);
+        this.trendingPage.update((prevPage) => prevPage + 1);
+        this.isLoadingTrendingGifs.set(false);
       });
   }
 
